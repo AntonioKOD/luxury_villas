38b3984b69c1de68e54e9c2be582a3a25267ccc0
Antonio Kodheli <antonio_kodheli@icloud.com>
2025-03-28 01:20:19 -0400

    added lazy loading

--- app/actions.ts
+++ app/actions.ts
@@ -6,10 +6,13 @@ import config from '@payload-config'
 const payload = getPayload({config})
 
 
-export const getProperties = async () => {
+export const getProperties = async (page=1, limit=10) => {
     const properties = await (await payload).find({
         collection: 'properties',
         depth: 3,
+        limit,
+        page,
+        sort: 'name'
     })
 
     return properties
