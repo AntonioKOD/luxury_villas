79b8c638914728a27591bd6e40b1cc33e0e3943e
Antonio Kodheli <antonio_kodheli@icloud.com>
2025-05-05 15:44:27 -0400

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-//import Navbar from "@/components/navbar";
+import Navbar from "@/components/navbar";
 import { Bodoni_Moda } from 'next/font/google';
 import "./globals.css";
-//import Footer from "@/components/footer";
-//import Exclusion from "@/components/Exclusion";
-//import SuspenseWrapper from "@/components/SuspenseWrapper"; // our client component
-import PaymentRequiredPage from "@/components/dev";
+import Footer from "@/components/footer";
+import Exclusion from "@/components/Exclusion";
+import SuspenseWrapper from "@/components/SuspenseWrapper"; // our client component
+
 
 const bodoni = Bodoni_Moda({
   subsets: ['latin'],
@@ -29,13 +29,22 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} ${bodoni.className} antialiased`}>
-       <PaymentRequiredPage/>
+       <Exclusion>
+          <Navbar />
+        </Exclusion> 
+        <SuspenseWrapper>
+          {children}
+        </SuspenseWrapper>
+        <Exclusion>
+          <Footer />
+        </Exclusion>
       </body>
     </html>
   );
